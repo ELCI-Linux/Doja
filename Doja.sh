804221e11b53fe6a93537150843f2711c9cb284a
ELCI-Linux <88078492+ELCI-Linux@users.noreply.github.com>
2021-12-21 18:32:05 +0000

    Update Doja.sh

--- Doja.sh
+++ Doja.sh
@@ -20,12 +20,11 @@ vers='1.0'
         linenum=$(cat songs/$selection.txt | wc -l )
         # select a random number between 1 and nth number of lines
 	 numline=$(shuf -i 1-$linenum -n 1)
-	echo line selection was $numline
-        # print line
+	# print line
 	
 	lyrics=$(head -n $numline songs/$selection.txt | tail -4)
 	title=$(head -n $selection songlist.txt | tail -1)
-        text=$title:$lyrics
+        text=$title:$lyricsecho
 	echo $text | cowsay
 	# exit
         exit
